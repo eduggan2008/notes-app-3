4c008c4c01e6c09a53efa05e5dfd2581a6c8e67e
Edward Duggan <eduggan2008@gmail.com>
2024-12-01 07:53:41 -0500

    added Amplify Auth, Data, Storage and Cloud Sandbox

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={reactLogo} className="logo react" alt="React logo" />
 
-        <h1>Hello from Amplify</h1>
+        <h1>Hello from AWS Amplify</h1>
+        <h2>Added AWS Amplify Auth, Data and Storage</h2>
+        <h3>Added AWS Amplify Cloud Sandbox</h3>
       </header>
     </div>
   );
